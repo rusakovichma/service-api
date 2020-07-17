8d42d26377806373df2677e70b9f2080b78465e6
Mikhail Rusakovich <mikhail_rusakovich@epam.com>
2020-07-17 22:22:01 +0300

    get Item Activities Authorization Test

--- src/test/java/com/epam/ta/reportportal/ws/controller/ActivityControllerTest.java
+++ src/test/java/com/epam/ta/reportportal/ws/controller/ActivityControllerTest.java
@@ -20,6 +20,8 @@ import com.epam.ta.reportportal.ws.BaseMvcTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.context.jdbc.Sql;
 
+import java.util.List;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -29,37 +31,51 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @Sql("/db/activity/activity-fill.sql")
 class ActivityControllerTest extends BaseMvcTest {
 
-	@Test
-	void getActivityByWrongTestItemId() throws Exception {
-		mockMvc.perform(get(DEFAULT_PROJECT_BASE_URL + "/activity/1111").with(token(oAuthHelper.getDefaultToken())))
-				.andExpect(status().is(404));
-	}
+    @Test
+    void getActivityByWrongTestItemId() throws Exception {
+        mockMvc.perform(get(DEFAULT_PROJECT_BASE_URL + "/activity/1111").with(token(oAuthHelper.getDefaultToken())))
+                .andExpect(status().is(404));
+    }
 
-	@Test
-	void getActivityByWrongProjectName() throws Exception {
-		mockMvc.perform(get("/v1/wrong_project/activity/1").with(token(oAuthHelper.getDefaultToken()))).andExpect(status().is(403));
-	}
+    @Test
+    void getActivityByWrongProjectName() throws Exception {
+        mockMvc.perform(get("/v1/wrong_project/activity/1").with(token(oAuthHelper.getDefaultToken()))).andExpect(status().is(403));
+    }
 
-	@Test
-	void getTestItemActivitiesByWrongTestItem() throws Exception {
-		mockMvc.perform(get(DEFAULT_PROJECT_BASE_URL + "/activity/item/1111").with(token(oAuthHelper.getDefaultToken())))
-				.andExpect(status().is(404));
-	}
+    @Test
+    void getTestItemActivitiesByWrongTestItem() throws Exception {
+        mockMvc.perform(get(DEFAULT_PROJECT_BASE_URL + "/activity/item/1111").with(token(oAuthHelper.getDefaultToken())))
+                .andExpect(status().is(404));
+    }
 
-	@Test
-	void getTestItemActivitiesPositive() throws Exception {
-		mockMvc.perform(get(DEFAULT_PROJECT_BASE_URL + "/activity/item/1").with(token(oAuthHelper.getDefaultToken())))
-				.andExpect(status().is(200));
-	}
+    @Test
+    void getTestItemActivitiesPositive() throws Exception {
+        mockMvc.perform(get(DEFAULT_PROJECT_BASE_URL + "/activity/item/1").with(token(oAuthHelper.getDefaultToken())))
+                .andExpect(status().is(200));
+    }
 
-	@Test
-	void getActivityPositive() throws Exception {
-		mockMvc.perform(get(DEFAULT_PROJECT_BASE_URL + "/activity/1").with(token(oAuthHelper.getDefaultToken())))
-				.andExpect(status().is(200));
-	}
+    @Test
+    void getActivityPositive() throws Exception {
+        mockMvc.perform(get(DEFAULT_PROJECT_BASE_URL + "/activity/1").with(token(oAuthHelper.getDefaultToken())))
+                .andExpect(status().is(200));
+    }
 
-	@Test
-	void getActivitiesForProject() throws Exception {
-		mockMvc.perform(get(DEFAULT_PROJECT_BASE_URL + "/activity").with(token(oAuthHelper.getDefaultToken()))).andExpect(status().is(200));
-	}
+    @Test
+    void getActivitiesForProject() throws Exception {
+        mockMvc.perform(get(DEFAULT_PROJECT_BASE_URL + "/activity").with(token(oAuthHelper.getDefaultToken()))).andExpect(status().is(200));
+    }
+
+    @Test
+    void getItemActivitiesAuthorizationTest() throws Exception {
+        final String itemActivitiesUrl = DEFAULT_PROJECT_BASE_URL + "/activity/item/1";
+
+        mockMvc.perform(get(itemActivitiesUrl).with(token(oAuthHelper.getAnonymousToken())))
+                .andExpect(status().is(401));
+
+        String[] anotherProjectMembers = {oAuthHelper.getProject2MemberToken(), oAuthHelper.getProject2CustomerToken()};
+        for (String anotherProjectMemberToken : anotherProjectMembers) {
+            mockMvc.perform(get(itemActivitiesUrl).with(token(anotherProjectMemberToken)))
+                    .andExpect(status().is(403));
+        }
+    }
 }
